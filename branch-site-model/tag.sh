c9dbdf14da1abcaef0b445c6a99a8a2ee34c73b5
Naira-ipt <71655065+Naira-ipt@users.noreply.github.com>
2023-05-08 14:31:19 +0100

    Add files via upload

---
+++ branch-site-model/tag.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+################################################################
+#Please choose branches you want to Tag
+###############################################################
+sed -i 's/TUN114607/TUN114607 \#1/' L4_information.fasta.treefile
+sed -i 's/TUN118102/TUN118102 \#1/' L4_information.fasta.treefile
+sed -i 's/TUN122817/TUN122817 \#1/' L4_information.fasta.treefile
+sed -i 's/TUN129605/TUN129605 \#1/' L4_information.fasta.treefile
+sed -i 's/TUN134702/TUN134702 \#1/' L4_information.fasta.treefile
+sed -i 's/TUN18016/TUN18016 \#1/' L4_information.fasta.treefile
+sed -i 's/TUN191701/TUN191701 \#1/' L4_information.fasta.treefile
+sed -i 's/TUN193008/TUN193008 \#1/' L4_information.fasta.treefile
+sed -i 's/TUN206901/TUN206901 \#1/' L4_information.fasta.treefile
+sed -i 's/TUN222006/TUN222006 \#1/' L4_information.fasta.treefile
+sed -i 's/TUN228403/TUN228403 \#1/' L4_information.fasta.treefile
+sed -i 's/TUN252604/TUN252604 \#1/' L4_information.fasta.treefile
+sed -i 's/TUN30613/TUN30613 \#1/' L4_information.fasta.treefile
+sed -i 's/TUN66017/TUN66017 \#1/' L4_information.fasta.treefile
+sed -i 's/TUN74005/TUN74005 \#1/' L4_information.fasta.treefile
+sed -i 's/TUN81807/TUN81807 \#1/' L4_information.fasta.treefile
+sed -i 's/TUN93215/TUN93215 \#1/' L4_information.fasta.treefile
+sed -i 's/TUN3014585/TUN3014585 \#1/' L4_information.fasta.treefile
+sed -i 's/TUN3014597/TUN3014597 \#1/' L4_information.fasta.treefile
+sed -i 's/TUN3014591/TUN3014591 \#1/' L4_information.fasta.treefile
+sed -i 's/TUN3014598/TUN3014598 \#1/' L4_information.fasta.treefile
+sed -i 's/TUN3014618/TUN3014618 \#1/' L4_information.fasta.treefile
+sed -i 's/TUN3014593/TUN3014593 \#1/' L4_information.fasta.treefile
+sed -i 's/TUN3014601/TUN3014601 \#1/' L4_information.fasta.treefile
+sed -i 's/TUN3014596/TUN3014596 \#1/' L4_information.fasta.treefile
+sed -i 's/TUN3014602/TUN3014602 \#1/' L4_information.fasta.treefile
+
+
+
+
+
+
